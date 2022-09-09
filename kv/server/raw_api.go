c17f8bd204b66fa79440f2bbc1582a535aef2922
MaoChongxin <chongxin.mao@shopee.com>
2022-09-09 16:01:03 +0800

    merge origin && refactor

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,27 +12,84 @@ import (
 
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
-	// Your Code Here (1).
-	return nil, nil
+	rsp := &kvrpcpb.RawGetResponse{}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		rsp.Error = err.Error()
+		return rsp, nil
+	}
+
+	val, err := reader.GetCF(req.Cf, req.Key)
+	if err != nil {
+		rsp.Error = err.Error()
+		return rsp, nil
+	}
+
+	if val == nil {
+		rsp.NotFound = true
+		return rsp, nil
+	}
+	rsp.Value = val
+	return rsp, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
-	// Your Code Here (1).
-	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+	rsp := &kvrpcpb.RawPutResponse{}
+	err := server.storage.Write(req.Context, []storage.Modify{
+		{
+			Data: storage.Put{
+				Key:   req.Key,
+				Value: req.Value,
+				Cf:    req.Cf,
+			},
+		},
+	})
+	if err != nil {
+		rsp.Error = err.Error()
+	}
+	return rsp, nil
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
-	// Your Code Here (1).
-	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+	rsp := &kvrpcpb.RawDeleteResponse{}
+	err := server.storage.Write(req.Context, []storage.Modify{
+		{
+			Data: storage.Put{
+				Key: req.Key,
+				Cf:  req.Cf,
+			},
+		},
+	})
+
+	if err != nil {
+		rsp.Error = err.Error()
+	}
+	return rsp, nil
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
-	// Your Code Here (1).
-	// Hint: Consider using reader.IterCF
-	return nil, nil
+	rsp := &kvrpcpb.RawScanResponse{}
+	reader, err := server.storage.Reader(req.Context)
+	defer reader.Close()
+	if err != nil {
+		rsp.Error = err.Error()
+		return rsp, nil
+	}
+	dbIter := reader.IterCF(req.Cf)
+	defer dbIter.Close()
+	dbIter.Seek(req.StartKey)
+
+	for i := 0; dbIter.Valid() && i < int(req.Limit); i++ {
+		key := dbIter.Item().Key()
+		val, _ := dbIter.Item().Value()
+		rsp.Kvs = append(rsp.Kvs, &kvrpcpb.KvPair{
+			Key:   key,
+			Value: val,
+		})
+		dbIter.Next()
+	}
+	return rsp, nil
 }
